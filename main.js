b8860082db4663f03f2db1be66efd3c10d841b02
Asantae Shelton <64320195+Asantae@users.noreply.github.com>
2022-03-22 02:07:19 -0400

    updated javascript
    created the basic javascript methods and variable needed. Working on Arithmetic next and then error exceptions.

--- main.js
+++ main.js
@@ -1,54 +1,61 @@
-/* testing */
-
-console.log(document.querySelectorAll('.radio'));
-/* var radio = document.querySelectorAll('.radio');
-
-console.log(document.querySelectorAll('.customPerc')); 
-
-
-/* testing for loop */
-/*var i;
-for (i = 0; i < radio.length; i++) {
-    radio[i].onclick = function() {
-        if (radio.checked == true) {
-            return alert(isTrue)
-        }
-    }
+/*declares variables essential for the app to function */
+var bill = document.querySelector('#bill')
+var radio = document.querySelectorAll('.radio'); /*all radio values stored in an array */
+var customPercentage = document.querySelector('#custom');
+var numOfPeople = document.querySelector('#people');
+var percentage = 0;
+var people = 0;
+var textTip = 0;
+var textTotal = 0;
+var totalPrice = 0;
+document.getElementById('totalAmount').innerHTML = '$' + textTotal.toFixed(2); /* where the total output is store */
+document.getElementById('tipAmount').innerHTML = '$' + textTip.toFixed(2); /* where the tip output is stored */
+/* listens for any input */
+bill.addEventListener('input', updateValue);
+numOfPeople.addEventListener('input', updateTotal);
+for (var i = 0; i < radio.length; i++) {
+    radio[i].onclick = updateTip;
 }
-
-/* creates function that resets the calculator */
-
-var radio = document.getElementsByClassName('radio'); /*all radio values stored in an array */
-var textTotal = document.getElementById('totalAmount'); /* where the total output is store */
-var textTip = document.getElementById('totalTip'); /* where the tip output is stored */
-alert(radio.opt1.value);
-
-/* for (var i = 0; i < radio.length; i++) {
-    radio[i].onclick = function(){
-        textTotal.value = this.dataset.value;
-        
-        }
-} */
-var j = 0;
-radio[j].onclick = function () {
-    for (var j = 0; j < radio.length ; j++){
-        console.log('hi', j);
-    }
-}
-function reset()
-{
-
-}
-
-/* creates the function that will output the total price */
-function total()
-{
-   
-}
-
-/* creates the function that will output the total tip */
-function tip()
-{
+customPercentage.addEventListener('input', customTip);
+/* essential methods */
+function updateValue(inputBill){
+    textTotal = inputBill.target.value;
+    finalOutput();
     
+}
+
+function updateTotal(inputPeople){
+    people = inputPeople.target.value;
+    finalOutput();
+}
+
+/* this function cycles through the the radio buttons and takes the value of the buttion that  was clicked */
+function updateTip() {
+    for (var c = 0; c < radio.length; c++) {
+        while (radio[c].checked == true){
+            percentage = radio[c].defaultValue/100;
+            break;
+        }
+    }
+    textTip = textTotal * percentage;
+    finalOutput();
+}
+
+function customTip(){
 
 }
+
+/* this method outputs the total price and tip amount to the page */
+function finalOutput(){
+    parseFloat(totalPrice).toFixed(2);
+    totalPrice = (textTip + textTotal) / people;
+    console.log('totalPrice ' + totalPrice);
+    console.log('textTotal ' + textTotal);
+    console.log('textTip ' + textTip);
+
+    document.getElementById('tipAmount').innerHTML = '$' + parseFloat(textTip).toFixed(2);
+    
+    document.getElementById('totalAmount').innerHTML = '$' + parseFloat(totalPrice).toFixed(2);
+}
+
+/* create method that checks decimal places of input*/
